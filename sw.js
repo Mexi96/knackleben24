83aa44ad5afe191d1bf64e1bb94097c5039726dc
Mexi96 <max.buhss@gmail.com>
2026-09-01 17:21:01 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,5 @@
-const CACHE="knack-v8";
-const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png", "./knack-app-qr.png", "./logo-orange.png", "./logo-amber.png", "./logo-red.png", "./logo-dark.png", "./logo-blue.png", "./logo-cyan.png", "./logo-purple.png", "./logo-green.png", "./logo-ice.png", "./logo-pink.png", "./logo-light.png", "./logo-sand.png"];
+const CACHE="knack-v9";
+const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png", "./logo-pink.png", "./logo-green.png", "./logo-blue.png", "./logo-cyan.png", "./logo-red.png", "./logo-amber.png", "./logo-dark.png", "./logo-light.png", "./logo-purple.png", "./logo-orange.png", "./logo-ice.png", "./logo-sand.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-  .then(()=>self.clients.claim())
-));
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));
-});
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));});
