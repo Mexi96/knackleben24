8d7965d3fef839e634f4d141c64cdde71a6a8202
Mexi96 <max.buhss@gmail.com>
2026-09-01 14:32:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="knacktracker-v3";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./logo.png","./icon-180.png","./icon-192.png","./icon-512.png"];
+const CACHE="knack-v4";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./logo-static.png","./icon-180.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
